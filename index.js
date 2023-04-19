fd32de514448527369c12e9809950fd80d862b04
csslick <digitone@naver.com>
2023-04-19 19:00:14 +0900

    계정확인 후 삭제

--- index.js
+++ index.js
@@ -35,7 +35,39 @@ MongoClient.connect(uri, async(err, client) => {
   app.listen(3001, () => {
     console.log(`server run at ${PORT}`)
   })
-  
+})
+
+// 로그인시 실행되는 미들웨어 
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pwd',
+  session: true,  // 로그인 후 세션 저장 유무
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (err,res) {
+    if (err) return done(err)
+    // 일치하는 아이디 없을 때
+    if (!res) return done(null, false, { message: '존재하지않는 아이디요' })
+    // 비번 채크
+    if (입력한비번 == res.pwd) {
+      return done(null, res)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
+
+// 로그인 성공시 id를 이용하여 세션을 저장하는 코드
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+// 세션데이터를 확인하여 DB에서 사용자 확인(마이페이지 접속시)
+passport.deserializeUser((id, done) => {
+  db.collection('login').findOne({id: id}, (err, res) => {
+    done(null, res)
+  })
 })
 
 
@@ -45,7 +77,7 @@ app.get('/', async (req, res)=> {
     count = await db.collection('post').count();
     res.render('index', { 
       posts: posts,
-      count: count 
+      count: count,
     })
   })
 })
@@ -55,24 +87,32 @@ app.get('/write', (req, res) => {
 })
 
 app.post('/add', (req, res) => {
-  console.log(req.body)
+  // console.log(req.body)
+  console.log("add: ", req.user.id)
   // db 저장
   db.collection('post').insertOne({
     title: req.body.title, 
-    date: req.body.date
+    date: req.body.date,
+    author: req.user.id,
   }, (err, res) => {
     console.log('저장완료');
   })
   res.redirect('/');
 })
 
+
+// 본인 글만 삭제처리: 글 _id와 작성자 확인
 app.post('/delete', (req, res) => {
-  const id = req.body.id;
+  console.log('user.id = ', req.user._id)
+  console.log('author: ', req.user.id);
+  const id = req.body.id; // post id
   const objectId = new ObjectID(id);
-  db.collection('post').deleteOne({_id: objectId}, (err, data) => {
-    if(err) return console.log(err)
-    console.log('delete id: ', id);
-    res.status(200).redirect('/');
+  db.collection('post')
+    .deleteOne({_id: objectId, author: req.user.id}, (err, data) => {
+      console.log('삭제완료');
+      if(err) return console.log(err)
+      console.log('delete id: ', id);
+      res.status(200).redirect('/');
   })
 })
 
@@ -139,38 +179,6 @@ function isLogin(req, res, next) {
   }
 }
 
-// 로그인시 실행되는 미들웨어 
-passport.use(new LocalStrategy({
-  usernameField: 'id',
-  passwordField: 'pwd',
-  session: true,  // 로그인 후 세션 저장 유무
-  passReqToCallback: false,
-}, function (입력한아이디, 입력한비번, done) {
-  //console.log(입력한아이디, 입력한비번);
-  db.collection('login').findOne({ id: 입력한아이디 }, function (err,res) {
-    if (err) return done(err)
-    // 일치하는 아이디 없을 때
-    if (!res) return done(null, false, { message: '존재하지않는 아이디요' })
-    // 비번 채크
-    if (입력한비번 == res.pwd) {
-      return done(null, res)
-    } else {
-      return done(null, false, { message: '비번틀렸어요' })
-    }
-  })
-}));
-
-// 로그인 성공시 id를 이용하여 세션을 저장하는 코드
-passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
-
-// 세션데이터를 확인하여 DB에서 사용자 확인(마이페이지 접속시)
-passport.deserializeUser((id, done) => {
-  db.collection('login').findOne({id: id}, (err, res) => {
-    done(null, res)
-  })
-})
 
 
 // app.get('/search', async(req, res) => {
@@ -214,5 +222,18 @@ app.get('/search', async(req, res) => {
       posts: posts,
       count: posts.length
     })
+  })
+})
+
+
+/*** 회원가입 
+ * passport 관련 함수 아래에 작성
+ * ***/
+app.post('/register', (req, res) => {
+  db.collection('login').insertOne({
+    id: req.body.id, 
+    pwd: req.body.pwd
+  }, (err, data) => {
+    res.redirect('/');
   })
 })
